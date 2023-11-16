2ece43eb1c4d37df0a656c55ccd491c609041d1d
Goreeeo <endmyaddress@gmail.com>
2023-11-16 16:46:26 +0100

    File Writing

--- ConcumaCompilerTerminal/Program.cs
+++ ConcumaCompilerTerminal/Program.cs
@@ -10,9 +10,27 @@ namespace ConcumaCompilerTerminal
         {
             while (true)
             {
+                string outFile = "";
+
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i] == "-o")
+                    {
+                        outFile = args[i + 1];
+                        break;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(outFile))
+                {
+                    Console.WriteLine("Need -o flag for output file.");
+                }
+
                 Tokenizer tokenizer = new(Console.ReadLine()!);
                 Parser parser = new(tokenizer.Lex());
                 Compiler compiler = new(parser.Parse());
+
+                File.WriteAllBytes(outFile, compiler.Compile().ToArray());
             }
         }
     }
