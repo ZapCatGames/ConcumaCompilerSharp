6344aad69871d7a4ad0598fccec31a58a6b5c83b
Goreeeo <endmyaddress@gmail.com>
2023-11-16 17:01:50 +0100

    Terminal Fix

--- ConcumaCompilerTerminal/Program.cs
+++ ConcumaCompilerTerminal/Program.cs
@@ -10,27 +10,42 @@ namespace ConcumaCompilerTerminal
         {
             while (true)
             {
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Not enough args.");
+                    return;
+                }
+
                 string outFile = "";
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine("File does not exist.");
+                    return;
+                }
+                string content = File.ReadAllText(args[0]);
 
                 for (int i = 1; i < args.Length; i++)
                 {
                     if (args[i] == "-o")
                     {
-                        outFile = args[i + 1];
-                        break;
+                        outFile = args[++i];
+                        continue;
                     }
                 }
 
                 if (string.IsNullOrEmpty(outFile))
                 {
                     Console.WriteLine("Need -o flag for output file.");
+                    return;
                 }
 
-                Tokenizer tokenizer = new(Console.ReadLine()!);
+                Tokenizer tokenizer = new(content);
                 Parser parser = new(tokenizer.Lex());
                 Compiler compiler = new(parser.Parse());
 
                 File.WriteAllBytes(outFile, compiler.Compile().ToArray());
+
+                Environment.Exit(0);
             }
         }
     }
